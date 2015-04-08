a0b6be71d576a62657be79ab1ff5a7d97523b740
Emily Newman <emily-jayne1@live.com>
2015-04-08 01:09:46 -0400

    work

---
+++ CompAssignments/a2/a2novice/InvoiceItem.java
@@ -0,0 +1,50 @@
+package a2novice;
+
+public class InvoiceItem {
+	private String name;
+	private double price_per_unit;
+	private int unit_count;
+	
+	public InvoiceItem(String name, double price_per_unit, int unit_count){
+	this.name = name.trim();
+	this.price_per_unit = price_per_unit;
+	this.unit_count = unit_count;
+	
+	if(price_per_unit<=0) {
+		throw new RuntimeException("Illegal invoice item");
+	}
+	if(unit_count<=0) {
+		throw new RuntimeException("Illegal invoice item");
+	}
+	for(int i=0; i<name.length(); i++) {
+	if(!Character.isLetterOrDigit(name.charAt(i))) {
+		throw new RuntimeException("Illegal invoice item");
+	}
+	}
+	if(!Character.isUpperCase(name.charAt(0))) {
+		throw new RuntimeException("Illegal invoice item");
+	}
+	if(name.equals(null)) {
+		throw new RuntimeException("Illegal invoice item");
+	}
+	}
+	public String getName() {
+		return name;
+	}
+	public double getPricePerUnit() {
+		return price_per_unit;
+	}
+	public int getUnitCount() {
+		return unit_count;	
+	}
+	public double getCost() {
+		return price_per_unit * unit_count;
+	}
+	//public static void main(String[] args) {
+		//InvoiceItem test = new InvoiceItem("Apple",1.7,2);
+		//System.out.println(test.getName());
+		//System.out.println(test.getPricePerUnit());
+		//System.out.println(test.getUnitCount());
+		//System.out.println(test.getCost());
+	//}
+}
